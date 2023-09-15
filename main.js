eb0f048f367c1fb97c12536ea641e0acbeeafc3b
Sergey Shirnin <66397172+sergey-shirnin@users.noreply.github.com>
2023-09-15 14:47:15 +1100

    Update main.js
    Stage 7

--- main.js
+++ main.js
@@ -3,19 +3,28 @@ const input = require('sync-input');
 const options = ['python', 'java', 'swift', 'javascript'];
 const word = options[Math.floor(Math.random() * options.length)];
 
-let attempts = 8, guessed = '', char = '', reg = '', masked = '', msg = '';
-const mask = '-', unique = new Set(word);
+let guessed = '', char = '', reg = '', masked = '', msg = '', attempts = 8, mask = '-';
 
 console.log('hangman'.toUpperCase().split('').join(' '));
 
-while (attempts > 0 & unique.size != guessed.length) {
+while (attempts > 0 & new Set(word).size != guessed.length) {
   regex = RegExp(['[^', guessed, ']'].join(''), 'g');
   masked = word.replace(regex, mask);
+
   char = input(`\n${masked}\nInput a letter: `);
-  msg = guessed.includes(char) ? 'No improvements.' : 
+  
+  msg = char.length != 1 ? 'Please, input a single letter.' : 
+    guessed.includes(char) ? 'You\'ve already guessed this letter.' :
+    (/[^a-z]/).test(char) ? 'Please, enter a lowercase letter from the English alphabet.' :
     word.includes(char) ? '' : 'That letter doesn\'t appear in the word.';
-  if (msg) { console.log(msg); }
-  msg ? attempts-- : guessed += char;
+  
+  if (msg) {
+    console.log(msg);
+  }
+  
+  msg === 'That letter doesn\'t appear in the word.' ? attempts-- : 
+    msg ? guessed += '' : guessed += char;
 }
 
-console.log(attempts ? `${word}\nYou guessed the word!\nYou survived!` : 'You lost!')
+console.log(new Set(word).size == guessed.length ? 
+            `\nYou guessed the word ${word}!\nYou survived!` : 'You lost!')
